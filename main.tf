91489fbf0ac4abd2d470a1af193afe65e7bb79df
Liron Nahshon <liron.nahshon@forcepoint.com>
2022-04-07 12:30:27 +0300

    public true to subnets

--- main.tf
+++ main.tf
@@ -25,6 +25,7 @@ resource "aws_subnet" "subnet1" {
  	 vpc_id           	 = aws_vpc.vpc.id
  	 cidr_block      	 = "10.0.16.0/20"
  	 availability_zone 	 = "us-east-1a"
+	 map_public_ip_on_launch = true
 
  	 tags = {
    		 Name = "subnet1"
@@ -37,6 +38,7 @@ resource "aws_subnet" "subnet2" {
 	vpc_id          	 = aws_vpc.vpc.id
 	cidr_block       	 = "10.0.32.0/20"
 	availability_zone	 = "us-east-1b"
+	map_public_ip_on_launch = true
 
 	tags = {
    		Name = "subnet2"
